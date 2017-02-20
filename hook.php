04ad8b582a256b8c825b0c8fdb0510c71fbbf486
Jacob <jacob.ralph@diamondprofilellc.com>
2017-02-20 01:44:25 -0500

    Fixed hook.
    Fixed hook path.

--- hook.php
+++ hook.php
@@ -1,6 +1,6 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-  echo shell_exec( 'cd /var/www/html/staging/vrapp/github/vridiot.github.io/ && sudo git reset --hard HEAD && sudo git pull' );
+  echo shell_exec( 'cd /var/www/html/staging/vrapp/minecraft/AltMinecraftRoom/ && sudo git reset --hard HEAD && sudo git pull' );
   $myfile = fopen("lastupdate.txt", "w") or die("Unable to open file!");
   $txt = time ();
   fwrite($myfile, $txt);
